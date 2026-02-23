906ebfd67d221e335e9b07ae2e4d5d1b545bcf31
Josefjosefjosef <josef-zmrhal@seznam.cz>
2026-02-23 03:02:34 +0100

    Revert "fix: define iuIsProjectsRoute in correct scope (#523)"
    This reverts commit e1ec5ed000e75b2076fe64a8341a52af73f4f8db.

--- assets/app.js
+++ assets/app.js
@@ -44,6 +44,7 @@ window.addEventListener("unhandledrejection", (e) => {
       return false;
     }
   }
+  try { window.iuIsProjectsRoute = iuIsProjectsRoute; } catch(e){}
 
   function iuStripEmptyHash(){
     try{
@@ -11729,7 +11730,7 @@ Rádia jsou vytížená, takže to nemusí vyjít vždy, ale snaha je opravdová
     applySectionFromURL();
   }
 
-  if (iuIsProjectsRoute()) {
+  if (typeof window.iuIsProjectsRoute === "function" && window.iuIsProjectsRoute()) {
     if (document.readyState === 'loading'){
       document.addEventListener('DOMContentLoaded', initNavRouter);
     } else {
